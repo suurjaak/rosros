6888bf0d57999524185a1224f80f67b75c782340
Erki Suurjaak <erki@lap.ee>
2022-06-25 14:08:26 +0300

    Auto-import all rclify submodules.

--- src/rosros/rclify/__init__.py
+++ src/rosros/rclify/__init__.py
@@ -7,7 +7,7 @@ Released under the BSD License.
 
 @author      Erki Suurjaak
 @created     17.02.2022
-@modified    25.02.2022
+@modified    23.06.2022
 ------------------------------------------------------------------------------
 """
 ## @namespace rosros.rclify
@@ -15,14 +15,17 @@ import os
 
 
 if os.getenv("ROS_VERSION") != "2":
+    from rosros.rclify import callback_groups
     from rosros.rclify import clock
     from rosros.rclify import duration
     from rosros.rclify import exceptions
+    from rosros.rclify import executors
     from rosros.rclify import node
     from rosros.rclify import parameter
     from rosros.rclify import qos
     from rosros.rclify import task
     from rosros.rclify import time
+    from rosros.rclify import topic_endpoint_info
     from rosros.rclify.parameter import Parameter
     from rosros.rclify.task import Future
     from rosros.rclify.rclify import *
